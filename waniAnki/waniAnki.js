f6866f45b837ec9b77f1a58137d7943ef7f8890c
Damian <damian221sh@hotmail.com>
2021-10-13 22:42:22 -0600

    quick fix

--- waniAnki/waniAnki.js
+++ waniAnki/waniAnki.js
@@ -93,7 +93,7 @@ async function wanikani(){
  */ 
 async function kanjiJSON(){
 	var kanjiJS = "";
-	await $.getJSON("../json/japanese/kanji.json", function(json) {
+	await $.getJSON("https://damiansh.github.io/json/japanese/kanji.json", function(json) {
 		kanjiJS = json;
 	});			
 	return kanjiJS;
